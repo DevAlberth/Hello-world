ae2b315505d91d64563c2db9cf49b011a5d7c1a2
Alberth Brendon <alberth.brendon@gmail.com>
2025-04-05 15:01:02 -0300

    TAREFA DE VERSIONAMENTO DE CODIGO EBAC

---
+++ HELLOWORLD.java
@@ -0,0 +1,13 @@
+public class HELLOWORLD {
+    public static void main (String[] args){
+        String controldevString = args[0];
+        if (controldevString.equals ("DEVCONTROL")){
+            System.out.println("Hello" + " " + args[1] + " " + args[2] + "," + " " + "How are you?");   
+   
+    } else if (controldevString.equals ("TEST")){
+          System.out.println("Hello");
+         }
+    }
+
+}
+    
